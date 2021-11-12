877ce1c99002e5eaad070967913c40f145091622
MarynaSychova <90452852+MarynaSychova@users.noreply.github.com>
2021-11-12 17:23:57 +0200

    Update zadacha1.c

--- zadacha1.c
+++ zadacha1.c
@@ -1,1 +1,33 @@
+#include <stdio.h>
 
+int pow_K(int num);
+
+int main()
+{
+	int N;
+	
+	printf("N: ");
+	scanf("%i", &N);
+	
+	int mod;
+	
+	for (int K = 1; K < N; K++)
+	{
+	mod = pow_K(K);
+	if (mod > N)
+		{
+		printf("%i\n", K);
+		break;
+		}
+	}
+}
+
+int pow_K(int num)
+{
+	int product = 1;
+	for (int i = 0; i < 2; i++)
+	{
+		product *= num;
+	}
+	return product;
+}
